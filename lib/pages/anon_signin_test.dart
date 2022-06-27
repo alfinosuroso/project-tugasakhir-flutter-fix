1912e4d9f262d8424e76d58d77ce52b424b0ba31
alfinosuroso <alfinosuroso@gmail.com>
2022-06-27 19:48:55 +0700

    layout beranda coba

---
+++ lib/pages/anon_signin_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AnonSignInTest extends StatelessWidget {
+  const AnonSignInTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue.shade50,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //* TITLE
+            Text(
+              'SIGN IN ANONYMOUSLY',
+              style:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+
+            //* SIGN IN STATUS
+            // CODE HERE: Change status based on current user
+            const Text("You haven't signed in yet"),
+            const SizedBox(height: 15),
+
+            //* SIGN IN BUTTON
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.lightBlue.shade900)),
+                  onPressed: () {
+                    // CODE HERE: Sign in anonymously / Sign out from firebase
+                  },
+                  // CODE HERE: Change button text based on current user
+                  child: const Text("Sign In")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages_home/beranda.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class Beranda extends StatelessWidget {
+  const Beranda({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 200.0, // Set this height
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+
+          flexibleSpace: Container(
+            height: 200.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  height: 100.0,
+                  child: Center(
+                    child: Text(
+                      "Home",
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 80.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(1.0),
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(0.5), width: 1.0),
+                          color: Colors.white),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.menu,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                              // _scaffoldKey.currentState.openDrawer();
+                            },
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Search",
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              print("your menu action here");
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 200),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 200),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                  Text(
+                    "HOLAAA",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                
+                onPressed: () {},
+                child: Icon(Icons.plus_one),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+
+// Text("HOLAAA", style: TextStyle(fontSize: 100),),
